b8ccae2d73f67ec9c82d53683afceb5ea710bf19
lschimm <lschimmelfennig@gmail.com>
2019-12-10 14:25:17 -0600

    Adds lecture notes

--- Lecture_Notes/Tuesday-Lecture2.py
+++ Lecture_Notes/Tuesday-Lecture2.py
@@ -10,15 +10,36 @@ names = [
     "Houston"
 ]
 
-def linear_search(array, value):
+def linear_search(array, value): # Looks at EVERY item one by one
     for name in array:
         if name == value:
             return True
     return False
 
-def binary_search(array, value):
-    pass
+def binary_search(array, value): # starts at the middle and decides where to go from there
+    start = 0
+    end = len(array) - 1
+
+    found = False
+
+    while not found and start <= end: 
+        middle = (start + end) // 2
+
+        # if found break
+        if array[middle] == value:
+            found == True
+        else:
+            if value < array[middle]:
+                # search lower half
+                end = middle - 1
+            else: 
+                # search upper half
+                start = middle + 1
+        return found
 
 
 print(linear_search(names, "Henry"))
 print(linear_search(names, "Jin"))
+
+print(binary_search(names, "Cath"))
+print(binary_search(names, "Jin"))
