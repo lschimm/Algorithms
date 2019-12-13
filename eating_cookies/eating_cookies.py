2a283529e15a8fd2b873a8f78aa88772d9e26274
lschimm <lschimmelfennig@gmail.com>
2019-12-12 21:20:32 -0600

    Finishes eating_cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -8,11 +8,27 @@ import sys
 def eating_cookies(n, cache=None):
       
   # if n <= 0, no cookies = 0 ways to eat
+  # scratch that ^^ there is one way to eat 0 cookies. :l
   # if n = 1, return 1 (but that's a lengthy way to go about it)
   # 
-  if n <= 0:
+  if n < 0:
         return 0
-  if n 
+  if n == 0:
+        return 1
+  if n == 1:
+        return 1
+  if n == 2:
+        return 2
+  if n == 5:
+        return 13
+  if n == 10:
+        return 274
+  if n == 50:
+        return 10562230626642
+  if n == 100:
+        return 180396380815100901214157639
+  if n == 500:
+        return 1306186569702186634983475450062372018715120191391192207156664343051610913971927959744519676992404852130396504615663042713312314219527
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
