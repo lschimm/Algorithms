195cb025124d6e6e04b98812e5afe02feb55b86d
lschimm <lschimmelfennig@gmail.com>
2019-12-12 20:54:38 -0600

    finishes recipes. aaahhhhhhhh

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -9,19 +9,34 @@ def recipe_batches(recipe, ingredients):
   # So loop through it once to see if there's enough (subtract the recipe from the ingredients)
   # if so, loop again to see if there's enough remainding for other batches
   # if not, then stop looping and return the amount available
-
-
-
-  pass
-
-
-
   
+  total = 9999
+  #Iterating through the dictionary
+  for key, value in recipe.items():
+    #Comparing to see if the ingredients exist
+    if ingredients[key]:
+      #If it exist then get the modulo of recipe in ingredients.
+      temp = ingredients[key] / value
+      #assign the modulo value to total, if the modulo value is less than total, replace it.
+      if (temp < total):
+        total = temp
+      if (len(recipe) != len(ingredients)):
+            return 0
+    else:
+      return 0
+  return int(total)
+      
 
 
-  
 
 
+
+print(recipe_batches(
+  {'milk': 100, 'butter': 50, 'flour': 5},
+  {'milk': 150, 'butter': 70, 'flour': 51}
+))
+      
+ 
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
